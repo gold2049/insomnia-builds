90d7b1a7647a6756fe0791a0426afaf7ee386259
gold2049 <33188624+gold2049@users.noreply.github.com>
2017-12-02 16:56:54 -0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-client.on('ready', () => {
-console.log ('I am ready!');
-});
-
-client.on('message', message => {
-If(message.content === 'ping') {
-message.reply('pong');
-}
-});
+bot.login('Mzg1NDg5NzIyNjMxMjU4MTIy.DQCH5A.91bIw2WQ_JUE_Wk2DTy1TFpAVvs');
 
 // this must be this way
 client.login(process.env.BOT_TOKEN);
